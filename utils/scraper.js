dbe63ced8b14d7acd370bc9b840889bcb16c398f
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-23 00:28:02 +0200

    Finish event brite scraper

--- utils/scraper.js
+++ utils/scraper.js
@@ -41,39 +41,43 @@ export const scrapeEvents = async ({ browser, retryCount }) => {
   const scrapeEventbrite = async ({ browser, retryCount }) => {
     try {
         const page = await browser.newPage();
+
+        let eventCardSelector = '[data-event-bucket-label="Events near Harare in Harare"] .discover-vertical-event-card'
     
         try {
-          await page.goto("https://www.airbnb.com/", { waitUntil: "load" });
+          await page.goto("https://www.eventbrite.com/d/zimbabwe--harare/events/", { waitUntil: "load" });
     
-          await page.waitForSelector('[itemprop="itemListElement"]', {
+          await page.waitForSelector(eventCardSelector, {
             timeout: 10000,
           });
     
-          const listings = await page.$$eval(
-            '[itemprop="itemListElement"]',
+          const events = await page.$$eval(
+           eventCardSelector,
             (elements) => {
-              return elements.slice(0, 10).map((element) => {
-                const title =
-                  element.querySelector(".t1jojoys")?.innerText || "N/A";
-                const price =
-                  element.querySelector("._11jcbg2")?.innerText || "N/A";
-                const link = element.querySelector("a")?.href || "N/A";
-                return { title, price, link };
+              return elements.map((element) => {
+                const link =  element.querySelector("a.event-card-link")?.href || "N/A";
+                const image =  element.querySelector("a.event-card-image")?.src || "N/A";
+                const title = element.querySelector("h3")?.innerText || "N/A";
+                const location = element.querySelector("p.event-card__clamp-line--one")?.innerText || "N/A";
+                const date =  element.querySelector(".event-card-details p")?.innerText || "N/A";
+
+     
+                return { link, image, title, date, location};
               });
             }
           );
     
-          const validListings = listings.filter(validateListing);
+          const validEvents = events.filter(validateEvent);
     
-          if (validListings.length === 0) {
-            throw new Error("No listings found");
+          if (validEvents.length === 0) {
+            throw new Error("No events found");
           }
     
-          return validListings;
+          return validEvents;
         } catch (pageError) {
           if (retryCount < MAX_RETRIES) {
             console.log(`Retrying... (${retryCount + 1}/${MAX_RETRIES})`);
-            return await scrapeListings(retryCount + 1);
+            return await scrapeEventbrite(retryCount + 1);
           } else {
             throw new Error(
               `Failed to scrape data after ${MAX_RETRIES} attempts: ${pageError.message}`
