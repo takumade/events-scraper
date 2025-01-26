771fb501a217b1c1942d56b2b0c5ceefed7b0390
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-27 00:00:04 +0200

    Change bropwer to  ctx

--- backend/scraper.js
+++ backend/scraper.js
@@ -1,35 +1,28 @@
 // scraper.js
 
-import sites from "./sites.js"
-
+import sites from "./sites.js";
 
 import scrapeAllevents from "./scrappers/allevents.js";
 import scrapeEventbrite from "./scrappers/eventbrite.js";
 
+export const scrapeEvents = async (context, retryCount) => {
+  try {
+    let allEvents = [];
 
+    for (const site of sites) {
+      if (site.startsWith("https://www.eventbrite.com")) {
+        let result = await scrapeEventbrite(context, retryCount);
+        allEvents = allEvents.concat(result);
+      } else if (site.startsWith("https://allevents.in")) {
+        let result = await scrapeAllevents(context, retryCount);
+        allEvents = allEvents.concat(result);
+      } else {
+        throw new Error(`Unsupported site: ${site}`);
+      }
 
-export const scrapeEvents = async ({ browser, retryCount }) => {
-    try {
-
-        let allEvents = []
-
-        for (const site of sites){
-            if (site.startsWith("https://www.eventbrite.com")){
-                let result = await scrapeEventbrite({ browser, retryCount });
-                allEvents = allEvents.concat(result);
-            }else if (site.startsWith("https://allevents.in")){
-                let result = await scrapeAllevents({ browser, retryCount });
-                allEvents = allEvents.concat(result);
-            }else{
-                throw new Error(`Unsupported site: ${site}`);
-            }
-        }
-
-    }catch(error){
-        console.log(error);
+      console.log("All events: ", allEvents);
     }
-
+  } catch (error) {
+    console.log(error);
+  }
 };
-
-
-
