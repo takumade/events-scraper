55f078c06bdfd32b2b643dbc816030f56ebb180d
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-27 00:42:00 +0200

    Create new page  in crape events

--- backend/scraper.js
+++ backend/scraper.js
@@ -5,16 +5,18 @@ import sites from "./sites.js";
 import scrapeAllevents from "./scrappers/allevents.js";
 import scrapeEventbrite from "./scrappers/eventbrite.js";
 
-export const scrapeEvents = async (context, retryCount) => {
+export const scrapeEvents = async (browser, retryCount) => {
   try {
     let allEvents = [];
 
+    const page = await browser.newPage();
+
     for (const site of sites) {
       if (site.startsWith("https://www.eventbrite.com")) {
-        let result = await scrapeEventbrite(context, retryCount);
+        let result = await scrapeEventbrite(page, retryCount);
         allEvents = allEvents.concat(result);
       } else if (site.startsWith("https://allevents.in")) {
-        let result = await scrapeAllevents(context, retryCount);
+        let result = await scrapeAllevents(page, retryCount);
         allEvents = allEvents.concat(result);
       } else {
         throw new Error(`Unsupported site: ${site}`);
@@ -22,6 +24,10 @@ export const scrapeEvents = async (context, retryCount) => {
 
       console.log("All events: ", allEvents);
     }
+
+    
+
+    return allEvents;
   } catch (error) {
     console.log(error);
   }
