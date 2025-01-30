6ba81edd2e5c19cb76f01bd4cfe145eaa2b722da
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-30 23:48:51 +0200

    Chnage footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-blue-500 w-full py-4 text-white text-center">
       <p>
-        &copy; 2024 Airbnb Web Scraper by{" "}
+        &copy; {new Date().getFullYear()} Harare Scraper Scraper by{" "}
         <a href="https://mihailgaberov.com">Mihail Gaberov</a>{" "}
       </p>
     </footer>
