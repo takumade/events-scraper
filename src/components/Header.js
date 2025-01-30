ec118737cc81332e5630164a4cc9b30bed6dd066
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-30 23:10:20 +0200

    Add header code

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="bg-blue-500 w-full py-4 text-white text-center">
+      <h1 className="text-left p-4 md:p-0 md:text-center md:text-3xl font-bold">
+        Airbnb Listings
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
