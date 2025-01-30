55da3d30dacdc7ba882ccb449c23ac8684921a80
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-30 23:09:17 +0200

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-blue-500 w-full py-4 text-white text-center">
+      <p>
+        &copy; 2024 Airbnb Web Scraper by{" "}
+        <a href="https://mihailgaberov.com">Mihail Gaberov</a>{" "}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
