f27fca690d1bb148ef56b28ef0ce51f2bc3f4095
Takunda Madechangu <madechangu.takunda@gmail.com>
2025-01-30 23:06:36 +0200

    Change to events

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { useLocalStorage } from "@uidotdev/usehooks";
+import axios from "axios";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import RefreshButton from "./components/RefreshButton";
+import Grid from "./components/Grid";
+import Loader from "./components/Loader";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [events, setEvents] = useLocalStorage("properties", []);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchEvents = async () => {
+    setLoading(true);
+    setError("");
+    setEvents([]);
+
+    try {
+      const response = await axios.get("http://localhost:5001/scrape");
+      if (response.data.length === 0) {
+        throw new Error("No events found");
+      }
+      setEvents(response.data);
+    } catch (err) {
+      setError(
+        err.response?.data?.error ||
+          "Failed to fetch events. Please try again."
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    // if (events.length === 0) {
+    //   fetchListings();
+    // }
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <Header />
+      <RefreshButton callback={fetchListings} loading={loading} />
+      <main className="flex flex-col items-center justify-center flex-1 w-full px-4 relative">
+        {error && <p className="text-red-500">{error}</p>}
+        {loading ? <Loader /> : <Grid events={events} />}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
